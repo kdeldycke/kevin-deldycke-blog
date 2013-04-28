55a04ee0a6c1c843b4922e6362dca4165a59f160
Kevin Deldycke <kevin@deldycke.com>
2013-04-28 13:29:25 +0200

    Sync conf and utilities script with latest version from Pelican 3.2.

--- publishconf.py
+++ publishconf.py
@@ -1,18 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
 
+# This file is only used if you use `make publish` or
+# explicitly specify it as your config file.
+
+import os
 import sys
-sys.path.append('.')
+sys.path.append(os.curdir)
 from pelicanconf import *
 
 SITEURL = 'http://kevin.deldycke.com'
+RELATIVE_URLS = False
 
 DELETE_OUTPUT_DIRECTORY = True
-
-# Following items are often useful when publishing
-
-# Uncomment following line for absolute URLs in production:
-#RELATIVE_URLS = False
-
-#DISQUS_SITENAME = ""
-#GOOGLE_ANALYTICS = ""
