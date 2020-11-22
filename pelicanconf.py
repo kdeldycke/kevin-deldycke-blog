45e830ae5949bbff5cf838e9a5a266693cd92d16
Kevin Deldycke <kevin@deldycke.com>
2020-11-22 11:29:25 +0100

    Add snippet to load up local Plumage copy for debugging.

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,6 +2,15 @@
 
 import plumage
 
+# XXX Use local plumage copy for dev
+# from importlib.machinery import SourceFileLoader
+# from pathlib import Path
+# SourceFileLoader(
+#     "plumage",
+#     str(Path("../plumage/plumage/__init__.py").resolve())
+# ).load_module()
+
+
 SITEURL = "http://localhost:8000"
 RELATIVE_URLS = False
 
